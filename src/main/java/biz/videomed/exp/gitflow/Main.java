baeb781278495d4da4e296aa5bd221d0613971da
Paolo Contessi <paolo.contessi@gmail.com>
2015-08-28 10:30:29 +0200

    Implement Hello World main.

---
+++ src/main/java/biz/videomed/exp/gitflow/Main.java
@@ -0,0 +1,21 @@
+package biz.videomed.exp.gitflow;
+
+/**
+ * <p>
+ * .
+ * </p>
+ *
+ * @author <a href="mailto:software@videomed.biz">Videomed R&D Software
+ *         Dept.</a>
+ * @version 1.1-28/ago/2015
+ * @since 1.5.1.1
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.exit(0);
+	}
+
+}
